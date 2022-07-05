936d6e8e919cc55713b9a796e01545390b1564be
Liansbw <98090851+Liansbw@users.noreply.github.com>
2022-07-05 10:47:12 +0200

    update

---
+++ weatherapp/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+ 
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default{
+  name: "App"
+  data() {
+    return {
+      APIkey "6f2e71f43aa54f01a14730a91198aa67",
+      city: "Detroit",
+    };
+    created(){
+      this.getCurrentWeather()
+    }
+  }
+  methods: {
+    getCurrentWeather() {
+      axios.get ("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+      ).then(res => {
+        console.log(res.data)
+      ;
+      )
+      });
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+{
+  margin: 0;
+  padding: 0;
+  box-sizing: border box;
+  font-family: "Quicksand"
+
+}
+</style>
